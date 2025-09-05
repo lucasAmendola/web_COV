3223db743813a727d39963b3f099284a76b2dcc4
Lucas Amendola <lukitasromanpiles7@gmail.com>
2025-09-05 11:08:18 -0300

    continue develop

--- src/app/secciones/familias/familias-section.ts
+++ src/app/secciones/familias/familias-section.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-import { RouterModule, RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { SmallBanner } from '../../small-banner/small-banner/small-banner';
 import { SimplySection } from '../../section-component/simply-section/simply-section';
 import { EspecialSection1 } from '../../especial-section-1/especial-section-1';
@@ -8,7 +8,7 @@ import { BgsComponent } from '../../bg-section/bgs-component/bgs-component';
 
 @Component({
   selector: 'app-familias-section',
-  imports: [RouterOutlet, SmallBanner, SimplySection, EspecialSection1, RouterModule, BgsComponent],
+  imports: [SmallBanner, SimplySection, EspecialSection1, RouterModule, BgsComponent],
   templateUrl: './familias-section.html',
   styleUrl: './familias-section.css'
 })
